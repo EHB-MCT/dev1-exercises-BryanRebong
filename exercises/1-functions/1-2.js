a0b953a5088f6c4ac043f05dc1cc6010763ca41a
Bryan Rebong <bryan.rebong@student.ehb.be>
2022-10-06 15:08:14 +0200

    1-2 Created a box with black and red lines

--- exercises/1-functions/1-2.js
+++ exercises/1-functions/1-2.js
@@ -5,20 +5,29 @@ let canvas = document.querySelector("canvas");
    canvas.height = window.innerHeight;
    let context = canvas.getContext("2d");
 
-drawRect();
+drawLine();
 
 function drawLine() {
+
+    context.lineWidth = 5;
+    context.strokeStyle = 'red';
+    context.beginPath();
+    context.moveTo(50, 50);
+    context.lineTo(150, 150);
+    context.moveTo(150, 50);
+    context.lineTo(50, 150);
+    context.stroke();
+
     context.lineWidth = 5;
     context.strokeStyle = 'black';
     context.beginPath();
-    context.moveTo(50, 50);
+    context.moveTo(47.5, 50);
     context.lineTo(150, 50);
     context.lineTo(150, 150);
-    context.lineTo(100, 150);
-    context.lineTo(175, 150);
-    context.lineTo(175, 250);
-    context.lineTo(50, 250);
+    context.lineTo(50, 150);
     context.lineTo(50, 50);
     context.stroke();
+
+    
 
 }
