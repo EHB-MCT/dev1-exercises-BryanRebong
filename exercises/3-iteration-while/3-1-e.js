5206c7fb972d36c7e2b14264ecf058b4e1c61284
Bryan Rebong <bryan.rebong@student.ehb.be>
2022-10-21 20:57:39 +0200

    Finished exercises 3-1-e

--- exercises/3-iteration-while/3-1-e.js
+++ exercises/3-iteration-while/3-1-e.js
@@ -13,10 +13,11 @@ function draw() {
 }
 
 function drawDiagonalGrid() {
-    let i = ;
-    while (i <= ) {
-        Utils.drawLine(, , , );
-        Utils.drawLine(, , , );
-        i = ;
+    let i = 0;
+    while (i < 7) {
+        let step = 25 * i;
+        Utils.drawLine(50 + step, 200 - step, 200 + step, 350 - step);
+        Utils.drawLine(50 + step, 200 + step, 200 + step, 50 + step);
+        i++;
     }
 }
