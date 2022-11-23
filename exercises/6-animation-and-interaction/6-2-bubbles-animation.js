ef4b100591b612e97d513b4a813b8cdc3e708a5c
Bryan Rebong <bryan.rebong@student.ehb.be>
2022-11-23 16:30:15 +0100

    Wednesday 23 commits

--- exercises/6-animation-and-interaction/6-2-bubbles-animation.js
+++ exercises/6-animation-and-interaction/6-2-bubbles-animation.js
@@ -6,11 +6,46 @@ import * as utils from "../../scripts/utils.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-drawBubble(100, 300, 50, 180);
+let xPos = [];
+let yPos; [];
+let sizes; [];
+
+let ySpeed = 5;
+
+/*Setup();
+
+function Setup() {
+    for(let 1 = 0; i < bubblesAmount; i++); {
+        xPos[i] = width / 2;
+        yPos[i] = height;
+        sizes[i] = utils.randomNumber(25, 50);
+    }
+    
+
+    update(); 
+}
+
+function update() {
+    context.fillStyle= 'white';
+    context.fillRect(0, 0, width, height);
+    
+    for(let i : 0; i < bubblesAmount; i++) { 
+    yPos[i] -= ySpeed[i];
+    xPos[i] -= 
+    drawBubble(xPos[i], yPos[i], sizes[i], 100);
+
+
+    requestAnimationFrame(update);
+}
+
+
 
 function drawBubble(x, y, size, hue) {
-    context.fillStyle = utils.hsla(hue, 50, 50);
+    context.fillStyle = utils.hsl(hue, 50, 50);
     utils.fillCircle(x, y, size);
-    context.fillStyle = utils.hsla(hue, 50, 75);
+    context.fillStyle = utils.hsl(hue, 50, 75);
     utils.fillCircle(x + size / 3, y - size / 3, size / 4);
 }
+
+}
+*/
