a376afe18ef1067487186031a85fb39e88871de5
Bryan Rebong <bryan.rebong@student.ehb.be>
2023-08-09 16:44:43 +0200

    Finished exercise 1-2

--- exercises/1-functions/1-2.js
+++ exercises/1-functions/1-2.js
@@ -1,33 +1,31 @@
 "Use strict"
 
 let canvas = document.querySelector("canvas");
-   canvas.width = window.innerWidth;
-   canvas.height = window.innerHeight;
-   let context = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
 
 drawLine();
 
 function drawLine() {
-
-    context.lineWidth = 5;
-    context.strokeStyle = 'red';
+//Red cross
     context.beginPath();
     context.moveTo(50, 50);
-    context.lineTo(150, 150);
-    context.moveTo(150, 50);
-    context.lineTo(50, 150);
+    context.lineTo(200, 200);
+    context.moveTo(200, 50);
+    context.lineTo(50, 200);
+    context.lineWidth = 5
+    context.strokeStyle = "red";
     context.stroke();
 
-    context.lineWidth = 5;
-    context.strokeStyle = 'black';
+//Black square
     context.beginPath();
-    context.moveTo(47.5, 50);
-    context.lineTo(150, 50);
-    context.lineTo(150, 150);
-    context.lineTo(50, 150);
+    context.moveTo(50, 50);
+    context.lineTo(200, 50);
+    context.lineTo(200, 200);
+    context.lineTo(50, 200);
     context.lineTo(50, 50);
+    context.lineWidth = 5;
+    context.strokeStyle = "black";
     context.stroke();
-
-    
-
 }
